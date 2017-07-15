64eea953cf129789caf9cc6b8c459da0afbc6f1e
Brian Staruk <brian@staruk.me>
2017-07-15 10:22:13 -0400

    add image to header to demo imports

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom'
+import headerImg from './images/starbase.png';
 
 const Header = props => {
   const projectTitle = props.projectTitle ? props.projectTitle : 'hi mom';
 
   return (
     <header id="header" className="wrapper">
+      <img src={headerImg} alt="starbase-react" />
       <h1>{projectTitle}</h1>
       <nav id="nav">
         <ul>
