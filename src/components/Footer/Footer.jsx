dd5b9060bb9f0806d50c530671d400b403d8b8d1
Brian Staruk <brian@staruk.me>
2017-07-15 07:51:47 -0400

    update footer repo href

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ export default class Footer extends React.Component {
   render() {
     return (
       <footer id="footer" className="wrapper">
-        <p>by <a href="https://brian.staruk.me">brian staruk</a> | <a href="https://github.com/bstaruk/starbase">check it out!</a></p>
+        <p>by <a href="https://brian.staruk.me">brian staruk</a> | <a href="https://github.com/bstaruk/starbase-react">check it out!</a></p>
       </footer>
     );
   }
