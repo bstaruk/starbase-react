a64468b81e03606966e41185c711f68d6c562596
Brian Staruk <brian@staruk.me>
2017-10-21 15:32:48 -0400

    cleanup postcss plugin imports

--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -1,7 +1,16 @@
 const webpackMerge = require('webpack-merge');
 const webpackConfigBase = require('./webpack.config.base.js');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const pathsHelper = require('./lib/paths-helper');
+
+// postcss plugins
+const postcssImport = require('postcss-import');
+const stylelint = require('stylelint');
+const postcssReporter = require('postcss-reporter');
+const postcssCssnext = require('postcss-cssnext');
+const postcssNested = require('postcss-nested');
+const postcssRemoveRoot = require('postcss-remove-root');
+const cssMqpacker = require('css-mqpacker');
 
 module.exports = webpackMerge(webpackConfigBase, {
   output: {
@@ -19,25 +28,23 @@ module.exports = webpackMerge(webpackConfigBase, {
               loader: 'postcss-loader',
               options: {
                 sourceMap: 'inline',
-                plugins: function () {
-                  return [
-                    require('postcss-import'),
-                    require('stylelint')(),
-                    require('postcss-reporter')(),
-                    require('postcss-cssnext')({
-                      features: {
-                        autoprefixer: {
-                          grid: false
-                        }
+                plugins: () => [
+                  postcssImport,
+                  stylelint(),
+                  postcssReporter(),
+                  postcssCssnext({
+                    features: {
+                      autoprefixer: {
+                        grid: false
                       }
-                    }),
-                    require('postcss-nested'),
-                    require('postcss-remove-root'),
-                    require('css-mqpacker')({
-                      sort: true
-                    })
-                  ];
-                }
+                    }
+                  }),
+                  postcssNested,
+                  postcssRemoveRoot,
+                  cssMqpacker({
+                    sort: true
+                  })
+                ]
               }
             }
           ]
--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -4,6 +4,16 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const pathsHelper = require('./lib/paths-helper');
 
+// postcss plugins
+const postcssImport = require('postcss-import');
+const stylelint = require('stylelint');
+const postcssReporter = require('postcss-reporter');
+const postcssCssnext = require('postcss-cssnext');
+const postcssNested = require('postcss-nested');
+const postcssRemoveRoot = require('postcss-remove-root');
+const cssMqpacker = require('css-mqpacker');
+const cssnano = require('cssnano');
+
 module.exports = webpackMerge(webpackConfigBase, {
   module: {
     rules: [
@@ -16,29 +26,27 @@ module.exports = webpackMerge(webpackConfigBase, {
             {
               loader: 'postcss-loader',
               options: {
-                plugins: function () {
-                  return [
-                    require('postcss-import'),
-                    require('stylelint')(),
-                    require('postcss-reporter')(),
-                    require('postcss-cssnext')({
-                      features: {
-                        autoprefixer: {
-                          grid: false
-                        }
+                plugins: () => [
+                  postcssImport,
+                  stylelint(),
+                  postcssReporter(),
+                  postcssCssnext({
+                    features: {
+                      autoprefixer: {
+                        grid: false
                       }
-                    }),
-                    require('postcss-nested'),
-                    require('postcss-remove-root'),
-                    require('css-mqpacker')({
-                      sort: true
-                    }),
-                    require('cssnano')({
-                      autoprefixer: false,
-                      safe: true
-                    })
-                  ];
-                }
+                    }
+                  }),
+                  postcssNested,
+                  postcssRemoveRoot,
+                  cssMqpacker({
+                    sort: true
+                  }),
+                  cssnano({
+                    autoprefixer: false,
+                    safe: true
+                  })
+                ]
               }
             }
           ]
