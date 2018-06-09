7e73f7b74a6077ebe80b0f3fac77d22a4a4f59f1
Brian Staruk <brian@staruk.me>
2018-06-09 10:39:46 -0400

    footer verbiage updates

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => (
   <footer role="contentinfo" className="footer">
-    <p>crafted in boston by <a href="https://brian.staruk.me" title="continue to brian.staruk.me" className="h-card">brian staruk</a> | <a href="https://github.com/bstaruk/starbase" title="learn more about starbase on github">starbase on github</a></p>
+    <p>crafted in boston by <a href="https://brian.staruk.me" title="continue to brian.staruk.me" className="h-card">brian staruk</a> | <a href="https://github.com/bstaruk/starbase-react" title="learn more about starbase-react on github">starbase-react on github</a></p>
   </footer>
 );
 
