6092d318a4051ef18cbc0cd84d167cfffc4774d3
Brian Staruk <brian@staruk.net>
2020-08-14 08:17:20 -0400

    global styles buildout

--- src/global-styles.js
+++ src/global-styles.js
@@ -9,22 +9,18 @@ const GlobalStyle = createGlobalStyle`
   html {
     height: 100%;
     width: 100%;
-    text-rendering: optimizeLegibility;
   }
 
   body {
+    max-width: 60rem; /* 960/16 */
     font-family: ${(props) => props.theme.fonts.default};
     background-color: ${(props) => props.theme.color.white};
-    color: ${(props) => props.theme.color.mineshaft};
-  }
-
-  h1,
-  h2,
-  h3,
-  h4,
-  h5,
-  h6 {
-    font-weight: normal;
+    color: ${(props) => props.theme.color.mineShaft};
+    font-size: 100%;
+    font-weight: 400;
+    line-height: 1.4;
+    padding: 1rem;
+    margin: 0 auto;
   }
 `;
 
