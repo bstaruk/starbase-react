35506f7d8d76823d18ef8df5002854281c58ebc2
Brian Staruk <brian@staruk.net>
2020-08-14 08:07:28 -0400

    postcss removed from the dependencies & build

--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -1,16 +1,5 @@
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 const { merge } = require('webpack-merge');
-
-// postcss plugins
-const cssMqpacker = require('css-mqpacker');
-const postcssExtend = require('postcss-extend');
-const postcssImport = require('postcss-import');
-const postcssNested = require('postcss-nested');
-const postcssPresetEnv = require('postcss-preset-env');
-const postcssRemoveRoot = require('postcss-remove-root');
-const postcssReporter = require('postcss-reporter');
-const stylelint = require('stylelint');
 
 // import base config
 const webpackConfigBase = require('./webpack.config.base.js');
@@ -32,51 +21,9 @@ module.exports = merge(webpackConfigBase, {
     rules: [
       {
         test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader',
-            options: { importLoaders: 1 },
-          },
-          {
-            loader: 'postcss-loader',
-            options: {
-              ident: 'postcss',
-              sourceMap: 'inline',
-              plugins: () => [
-                stylelint(),
-                postcssReporter(),
-                postcssImport({
-                  path: [path.resolve(__dirname, '../src')],
-                }),
-                postcssNested(),
-                postcssPresetEnv({
-                  stage: 1,
-                  features: {
-                    'custom-properties': {
-                      preserve: false,
-                    },
-                    'custom-media': {
-                      preserve: false,
-                    },
-                  },
-                }),
-                postcssExtend(),
-                postcssRemoveRoot(),
-                cssMqpacker({
-                  sort: true,
-                }),
-              ],
-            },
-          },
-        ],
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader'],
       },
     ],
   },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: '[name].css',
-      chunkFilename: '[id].css',
-    }),
-  ],
 });
--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -1,19 +1,6 @@
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OfflinePlugin = require('offline-plugin');
-const path = require('path');
 const { merge } = require('webpack-merge');
-
-// postcss plugins
-const cssMqpacker = require('css-mqpacker');
-const cssnano = require('cssnano');
-const postcssExtend = require('postcss-extend');
-const postcssImport = require('postcss-import');
-const postcssNested = require('postcss-nested');
-const postcssPresetEnv = require('postcss-preset-env');
-const postcssRemoveRoot = require('postcss-remove-root');
-const postcssReporter = require('postcss-reporter');
-const stylelint = require('stylelint');
 
 // import base config
 const webpackConfigBase = require('./webpack.config.base.js');
@@ -26,54 +13,12 @@ module.exports = merge(webpackConfigBase, {
     rules: [
       {
         test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader',
-            options: { importLoaders: 1 },
-          },
-          {
-            loader: 'postcss-loader',
-            options: {
-              ident: 'postcss',
-              plugins: () => [
-                stylelint(),
-                postcssReporter(),
-                postcssImport({
-                  path: [path.resolve(__dirname, '../src')],
-                }),
-                postcssNested(),
-                postcssPresetEnv({
-                  stage: 1,
-                  features: {
-                    'custom-properties': {
-                      preserve: false,
-                    },
-                    'custom-media': {
-                      preserve: false,
-                    },
-                  },
-                }),
-                postcssExtend(),
-                postcssRemoveRoot(),
-                cssMqpacker({
-                  sort: true,
-                }),
-                cssnano({
-                  preset: 'default',
-                }),
-              ],
-            },
-          },
-        ],
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader'],
       },
     ],
   },
   plugins: [
-    new MiniCssExtractPlugin({
-      filename: '[name].[hash:8].css',
-      chunkFilename: '[id].[hash:8].css',
-    }),
     new CleanWebpackPlugin(),
     new OfflinePlugin({
       AppCache: false,
