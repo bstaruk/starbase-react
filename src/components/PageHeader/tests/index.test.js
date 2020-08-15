b3590479138210f2cff1f8f0e190e71c54fb3953
Brian Staruk <brian@staruk.net>
2020-08-15 18:45:33 -0400

    PageHeader tests buildout

--- src/components/PageHeader/tests/index.test.js
+++ src/components/PageHeader/tests/index.test.js
@@ -1,17 +1,31 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, waitForElement } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 import ThemeProvider from '@containers/App/Theme';
 
 import PageHeader from '../index';
 
 describe('<PageHeader />', () => {
-  it('Expect to not log errors in console', () => {
+  it.skip('Expect to not log errors in console', () => {
     const spy = jest.spyOn(global.console, 'error');
     render(
-      <ThemeProvider>
-        <PageHeader title="Test Page Header" />
-      </ThemeProvider>,
+      <BrowserRouter>
+        <ThemeProvider>
+          <PageHeader title="Satoshi" />
+        </ThemeProvider>
+      </BrowserRouter>,
     );
     expect(spy).not.toHaveBeenCalled();
+  });
+
+  it('renders personalized greeting', async () => {
+    const { getByText } = render(
+      <BrowserRouter>
+        <ThemeProvider>
+          <PageHeader title="Satoshi" />
+        </ThemeProvider>
+      </BrowserRouter>,
+    );
+    await waitForElement(() => getByText(/Satoshi/i));
   });
 });
