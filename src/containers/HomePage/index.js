ba8171662874fe9fa7a3f5893f66e02dac6d7004
Brian Staruk <brian@staruk.net>
2020-08-16 09:53:35 -0400

    hooking store into HomePage

--- src/containers/HomePage/index.js
+++ src/containers/HomePage/index.js
@@ -1,14 +1,51 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import { Helmet } from 'react-helmet';
+import { compose } from 'redux';
+import { createStructuredSelector } from 'reselect';
 
-const HomePage = () => (
-  <>
-    <Helmet>
-      <title>Home</title>
-    </Helmet>
+import { getRepoDetailsAction } from './actions';
+import { selectRepoDetails, selectRepoDetailsLoaded } from './selectors';
 
-    <p>home</p>
-  </>
-);
+const HomePage = ({ getRepoDetails, repoDetails, repoDetailsLoaded }) => {
+  useEffect(() => {
+    if (!repoDetailsLoaded) {
+      getRepoDetails({ owner: 'bstaruk', repo: 'starbase-react' });
+    }
+  }, []);
 
-export default HomePage;
+  return (
+    <>
+      <Helmet>
+        <title>Home</title>
+      </Helmet>
+
+      <p>home</p>
+
+      {repoDetailsLoaded && <p>{repoDetails.name}</p>}
+    </>
+  );
+};
+
+HomePage.propTypes = {
+  getRepoDetails: PropTypes.func.isRequired,
+  repoDetails: PropTypes.object.isRequired,
+  repoDetailsLoaded: PropTypes.bool.isRequired,
+};
+
+const mapStateToProps = createStructuredSelector({
+  repoDetails: selectRepoDetails(),
+  repoDetailsLoaded: selectRepoDetailsLoaded(),
+});
+
+function mapDispatchToProps(dispatch) {
+  return {
+    getRepoDetails: ({ owner, repo }) =>
+      dispatch(getRepoDetailsAction({ owner, repo })),
+  };
+}
+
+const withConnect = connect(mapStateToProps, mapDispatchToProps);
+
+export default compose(withConnect)(HomePage);
