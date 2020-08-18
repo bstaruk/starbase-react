036932dcfebd865ffc8b11d11ba053b1db2b9f87
Brian Staruk <brian@staruk.net>
2020-08-18 12:50:56 -0400

    jest css mock init

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,7 @@ module.exports = {
     '^@utils/(.*)': '<rootDir>/src/utils/$1',
     '.*\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/internals/mocks/image.js',
+    '.*\\.(css|less|styl|scss|sass)$': '<rootDir>/internals/mocks/css.js',
   },
   setupFilesAfterEnv: ['<rootDir>/internals/testing/test-bundler.js'],
 };
