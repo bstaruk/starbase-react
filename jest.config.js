2d1b30c9e97bf3e965d88251cc08e2adc9850662
Brian Staruk <brian@staruk.net>
2020-08-18 15:24:59 -0400

    moving mocks folder into testing folder (cleaner, imo)

--- jest.config.js
+++ jest.config.js
@@ -3,8 +3,8 @@ module.exports = {
   moduleDirectories: ['node_modules', 'src'],
   moduleNameMapper: {
     '.*\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/internals/mocks/image.js',
-    '.*\\.(css|less|scss|sass)$': '<rootDir>/internals/mocks/css.js',
+      '<rootDir>/internals/testing/mocks/image.js',
+    '.*\\.(css|less|scss|sass)$': '<rootDir>/internals/testing/mocks/css.js',
   },
   setupFilesAfterEnv: ['<rootDir>/internals/testing/test-bundler.js'],
 };
