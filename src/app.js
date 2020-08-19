eceaf6a81b8ec15af0f0293a923a1813d3140cd6
Brian Staruk <brian@staruk.net>
2020-08-19 07:49:39 -0400

    connected-router init

--- src/app.js
+++ src/app.js
@@ -2,20 +2,21 @@ import '@babel/polyfill'; // keep at top for redux-saga generator support
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { ConnectedRouter } from 'connected-react-router';
 import * as OfflinePluginRuntime from 'offline-plugin/runtime';
 
 import App from 'containers/App';
+import history from 'utils/history';
 
 import configureStore from './store';
 
-const store = configureStore();
+const store = configureStore(history);
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <ConnectedRouter history={history}>
       <App />
-    </BrowserRouter>
+    </ConnectedRouter>
   </Provider>,
   document.getElementById('root'),
 );
