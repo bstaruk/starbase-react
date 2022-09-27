7bd15c38059a5df0cf18958a642070bce925a122
Brian Staruk <brian@staruk.net>
2022-09-27 12:27:57 -0400

    boilerplate components cleanup

--- src/components/Counter/index.tsx
+++ src/components/Counter/index.tsx
@@ -2,23 +2,32 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { selectCount } from 'data/counter/selectors';
 import { actions as counterActions } from 'data/counter/slice';
+import { Wrapper, ButtonWrapper, ValueWrapper } from './wrappers';
 
 const Counter = () => {
   const count = useSelector(selectCount);
   const dispatch = useDispatch();
 
   return (
-    <div>
-      <div>
-        <button aria-label="Increment value" onClick={() => dispatch(counterActions.increment())}>
-          Increment
-        </button>
-        <span>{count}</span>
-        <button aria-label="Decrement value" onClick={() => dispatch(counterActions.decrement())}>
-          Decrement
-        </button>
-      </div>
-    </div>
+    <Wrapper>
+      <ButtonWrapper
+        aria-label="Decrement value"
+        onClick={() => dispatch(counterActions.decrement())}
+        disabled={count < 1}
+      >
+        Decrement
+      </ButtonWrapper>
+
+      <ValueWrapper>{count}</ValueWrapper>
+
+      <ButtonWrapper
+        aria-label="Increment value"
+        onClick={() => dispatch(counterActions.increment())}
+        disabled={count >= 10}
+      >
+        Increment
+      </ButtonWrapper>
+    </Wrapper>
   );
 };
 
