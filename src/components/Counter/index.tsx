4ae788c78d4ab918ea07adf36f6ae4ebc3aefd63
Brian Staruk <brian@staruk.net>
2022-09-28 14:13:36 -0400

    button component init

--- src/components/Counter/index.tsx
+++ src/components/Counter/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+
 import { selectCount } from 'data/app/selectors';
 import { actions as appActions } from 'data/app/slice';
-import { Wrapper, ButtonWrapper, ValueWrapper } from './wrappers';
+import Button from 'components/Button';
+import { Wrapper, ValueWrapper } from './wrappers';
 
 const Counter = () => {
   const count = useSelector(selectCount);
@@ -18,15 +20,15 @@ const Counter = () => {
 
   return (
     <Wrapper>
-      <ButtonWrapper aria-label="Decrement value" onClick={onMinusClick} disabled={count < 1}>
+      <Button ariaLabel="Decrement value" onClick={onMinusClick} disabled={count < 1}>
         Decrement
-      </ButtonWrapper>
+      </Button>
 
       <ValueWrapper>{count}</ValueWrapper>
 
-      <ButtonWrapper aria-label="Increment value" onClick={onPlusClick} disabled={count >= 10}>
+      <Button ariaLabel="Increment value" onClick={onPlusClick} disabled={count >= 10}>
         Increment
-      </ButtonWrapper>
+      </Button>
     </Wrapper>
   );
 };
--- src/components/Counter/wrappers.tsx
+++ src/components/Counter/wrappers.tsx
@@ -10,22 +10,6 @@ export const Wrapper = styled.div`
   }
 `;
 
-export const ButtonWrapper = styled.button`
-  padding: 8px 12px;
-  font-size: 17px;
-  background: none;
-  border: 3px solid ${p => p.theme.colors.white};
-  border-radius: 3px;
-  color: ${p => p.theme.colors.white};
-  cursor: pointer;
-
-  &:disabled {
-    cursor: not-allowed;
-    opacity: 0.5;
-    font-style: italic;
-  }
-`;
-
 export const ValueWrapper = styled.span`
   font-size: 17px;
   font-weight: 700;
