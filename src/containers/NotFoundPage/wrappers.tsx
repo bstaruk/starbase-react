21dbaa199a4e8c9fc2f587f55db79e37fdf11db0
Brian Staruk <brian@staruk.net>
2022-09-28 15:05:10 -0400

    404 page init

---
+++ src/containers/NotFoundPage/wrappers.tsx
@@ -0,0 +1,16 @@
+import styled from '@emotion/styled';
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+`;
+
+export const HeadingWrapper = styled.h1`
+  font-size: 42px;
+  font-weight: 700;
+  text-align: center;
+  margin-bottom: 20px;
+`;
