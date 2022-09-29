abc2b11c01ace5cc431692ef230400d8c4516280
Brian Staruk <brian@staruk.net>
2022-09-29 10:30:37 -0400

    remove container test

--- src/containers/HomePage/HomePage.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from 'testUtils';
-import HomePage from './index';
-
-test('renders hello world', () => {
-  render(<HomePage />);
-  const helloWorldElement = screen.getByText(/hello world/i);
-  expect(helloWorldElement).toBeInTheDocument();
-});
